435e46b1b8ec8c8c50fbe9ae55c37066d29bac0b
Sotirios Mantziaris <s.mantziaris@live.com>
2016-07-19 23:22:03 +0300

    added events

--- src/TradSim.EventAggregation/EventAggregation.fs
+++ src/TradSim.EventAggregation/EventAggregation.fs
@@ -1,19 +1,30 @@
 ﻿module TradSim.EventAggregation
 
+open System
 open Newtonsoft.Json
 
 type TradeDirection = Buy = 0 | Sell = 1
-type OrderStatus = Pending = 0 | PartiallyFilled =1 | FullyFilled = 2 | OverFilled =3
+type OrderStatus = Pending = 0 | PartiallyFilled = 1 | FullyFilled = 2 | OverFilled = 3
 
 type Order = { 
-    Id: System.Guid
+    Id: Guid
     Symbol: string
     Quantity: int
+    TradedQuantity : int
     Direction: TradeDirection
     UserId :int
-    Occured: System.DateTimeOffset
+    Occured: DateTimeOffset
     Status: OrderStatus
 }
+
+type Event = 
+    | OrderCreatePending of OrderID: string * Symbol: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
+    | OrderCreated of OrderID: string * Symbol: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
+    | OrderAmendPending of OrderID: Guid * Occured:DateTimeOffset * Version: int
+    | OrderAmended of OrderID: Guid * Occured:DateTimeOffset * Version: int
+    | OrderCancelPending of OrderID: Guid * Occured:DateTimeOffset * Version: int
+    | OrderCanceled of OrderID: Guid * Occured:DateTimeOffset * Version: int
+    | OrderTraded of OrderID: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
 
 let getJsonNetJson value = 
     sprintf "I used to be %s but now I'm %s!" value  (JsonConvert.SerializeObject(value))
