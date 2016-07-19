90dc5e845ec20ac1a0a3b46fd1e6bf6f4b56b073
Sotirios Mantziaris <s.mantziaris@live.com>
2016-07-19 23:59:31 +0300

    added application of event

--- src/TradSim.EventAggregation/EventAggregation.fs
+++ src/TradSim.EventAggregation/EventAggregation.fs
@@ -4,11 +4,18 @@ open System
 open Newtonsoft.Json
 
 type TradeDirection = Buy = 0 | Sell = 1
-type OrderStatus = Pending = 0 | PartiallyFilled = 1 | FullyFilled = 2 | OverFilled = 3
+type OrderStatus = Pending = 0 | PartiallyFilled = 1 | FullyFilled = 2 | OverFilled = 3 | Cancelled = 4
+
+let setOrderStatus quantity tradedQuantity =
+    if  tradedQuantity = 0 then OrderStatus.Pending
+    elif tradedQuantity < quantity then OrderStatus.PartiallyFilled
+    elif tradedQuantity = quantity then OrderStatus.FullyFilled
+    else OrderStatus.OverFilled     
 
 type Order = { 
     Id: Guid
     Symbol: string
+    Price: decimal 
     Quantity: int
     TradedQuantity : int
     Direction: TradeDirection
@@ -18,13 +25,25 @@ type Order = {
 }
 
 type Event = 
-    | OrderCreatePending of OrderID: string * Symbol: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
-    | OrderCreated of OrderID: string * Symbol: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
+    | OrderCreatePending of OrderID: Guid * Symbol: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
+    | OrderCreated of OrderID: Guid * Symbol: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
     | OrderAmendPending of OrderID: Guid * Occured:DateTimeOffset * Version: int
     | OrderAmended of OrderID: Guid * Occured:DateTimeOffset * Version: int
     | OrderCancelPending of OrderID: Guid * Occured:DateTimeOffset * Version: int
     | OrderCanceled of OrderID: Guid * Occured:DateTimeOffset * Version: int
-    | OrderTraded of OrderID: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
+    | OrderTraded of OrderID: Guid * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
+
+let apply item event = 
+    match item, event with
+        | None,    OrderCreatePending(orderID,symbol,price,quantity,direction,occured,version)  -> { Id = orderID; Symbol = symbol; Price = price; Quantity = quantity; TradedQuantity = 0; Direction = direction; UserId = 1; Occured= occured; Status= OrderStatus.Pending; }
+        | None,    _                                                                            -> raise (System.ArgumentException("unknown event"))
+        | Some(i), OrderCreated(orderID,symbol,price,quantity,direction,occured,version)        -> { i with Occured = occured; }        
+        | Some(i), OrderAmendPending(orderID,occured,version)                                   -> { i with Occured = occured; }
+        | Some(i), OrderAmended(orderID,occured,version)                                        -> { i with Occured = occured; }
+        | Some(i), OrderCancelPending(orderID,occured,version)                                  -> { i with Occured = occured; }
+        | Some(i), OrderCanceled(orderID,occured,version)                                       -> { i with Occured = occured; Status = OrderStatus.Cancelled }
+        | Some(i), OrderTraded(orderId,price,quantity,direction,occured,version)                -> { i with Occured = occured; TradedQuantity = i.TradedQuantity + quantity; Status = setOrderStatus i.Quantity (i.TradedQuantity + quantity) }        
+        | Some(i), _                                                                            -> i
 
 let getJsonNetJson value = 
     sprintf "I used to be %s but now I'm %s!" value  (JsonConvert.SerializeObject(value))
