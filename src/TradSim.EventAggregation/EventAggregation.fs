6272e02acbe5ba2af2669b67db987183245b5548
Sotirios Mantziaris <s.mantziaris@live.com>
2016-07-20 22:40:33 +0300

    added aggregate events

--- src/TradSim.EventAggregation/EventAggregation.fs
+++ src/TradSim.EventAggregation/EventAggregation.fs
@@ -26,7 +26,7 @@ type Order = {
 
 type Event = 
     | OrderCreatePending of OrderID: Guid * Symbol: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
-    | OrderCreated of OrderID: Guid * Symbol: string * Price: decimal * Quantity:int * Direction: TradeDirection * Occured:DateTimeOffset * Version: int
+    | OrderCreated of OrderID: Guid * Occured:DateTimeOffset * Version: int
     | OrderAmendPending of OrderID: Guid * Occured:DateTimeOffset * Version: int
     | OrderAmended of OrderID: Guid * Occured:DateTimeOffset * Version: int
     | OrderCancelPending of OrderID: Guid * Occured:DateTimeOffset * Version: int
@@ -36,14 +36,18 @@ type Event =
 let apply item event = 
     match item, event with
         | None,    OrderCreatePending(orderID,symbol,price,quantity,direction,occured,version)  -> { Id = orderID; Symbol = symbol; Price = price; Quantity = quantity; TradedQuantity = 0; Direction = direction; UserId = 1; Occured= occured; Status= OrderStatus.Pending; }
-        | None,    _                                                                            -> raise (System.ArgumentException("unknown event"))
-        | Some(i), OrderCreated(orderID,symbol,price,quantity,direction,occured,version)        -> { i with Occured = occured; }        
+        | None,    _                                                                            -> raise (ArgumentException("unknown event"))
+        | Some(i), OrderCreated(orderID,occured,version)                                        -> { i with Occured = occured; }        
         | Some(i), OrderAmendPending(orderID,occured,version)                                   -> { i with Occured = occured; }
         | Some(i), OrderAmended(orderID,occured,version)                                        -> { i with Occured = occured; }
         | Some(i), OrderCancelPending(orderID,occured,version)                                  -> { i with Occured = occured; }
         | Some(i), OrderCanceled(orderID,occured,version)                                       -> { i with Occured = occured; Status = OrderStatus.Cancelled }
         | Some(i), OrderTraded(orderId,price,quantity,direction,occured,version)                -> { i with Occured = occured; TradedQuantity = i.TradedQuantity + quantity; Status = setOrderStatus i.Quantity (i.TradedQuantity + quantity) }        
         | Some(i), _                                                                            -> i
+
+
+let aggregate events = 
+    events |> Seq.fold (fun acc e -> Some(apply acc e)) None
 
 let getJsonNetJson value = 
     sprintf "I used to be %s but now I'm %s!" value  (JsonConvert.SerializeObject(value))
