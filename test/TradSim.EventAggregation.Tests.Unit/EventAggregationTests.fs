e3f9d54c35b7da14f3cbf8a34db9d039b977bb9b
Sotirios Mantziaris <s.mantziaris@live.com>
2016-08-15 16:56:55 +0300

    fixed names and value literals

--- test/TradSim.EventAggregation.Tests.Unit/EventAggregationTests.fs
+++ test/TradSim.EventAggregation.Tests.Unit/EventAggregationTests.fs
@@ -8,10 +8,10 @@ open TradSim.EventAggregation
 let ``Order with Create Pending Event Received Resulting To Pending``() =
     let id = Guid.NewGuid()
     let occured = DateTimeOffset.UtcNow
-    let expected = {Id = id; Symbol = "TT"; Price= 10.0m; Quantity = 11; TradedQuantity = 0; 
+    let expected = {Id = id; Symbol = "TT"; Price= 10.0m; Quantity = 11u; TradedQuantity = 0u; 
                     Direction= TradeDirection.Buy; UserId =1; Occured= occured ; 
                     Status= OrderStatus.Pending; Trades= List.empty}
-    let accepted = OrderAccepted(id, "TT", 10.0m, 11, TradeDirection.Buy, occured, 1 )
+    let accepted = OrderAccepted(id, "TT", 10.0m, 11u, TradeDirection.Buy, occured, 1u )
     let actual = seq { yield accepted } |> aggregate
 
     Assert.Equal(expected, actual.Value)
@@ -20,7 +20,7 @@ let ``Order with Create Pending Event Received Resulting To Pending``() =
 let ``Order with Invalid Event Received on empty Order Resulting In Exception``() =
     let id = Guid.NewGuid()
     let occured = DateTimeOffset.UtcNow
-    let canceled = OrderCanceled(id, occured, 1 )
+    let canceled = OrderCancelled(id, occured, 1u )
 
     let exc = Assert.Throws<ArgumentException>(fun () -> seq { yield canceled } |> aggregate |> ignore)
     Assert.Equal("unknown event",exc.Message)
@@ -29,12 +29,12 @@ let ``Order with Invalid Event Received on empty Order Resulting In Exception``(
 let ``Order with Create Pending and Traded Event Received Resulting To PartiallyFilled``() =
     let id = Guid.NewGuid()
     let occured = DateTimeOffset.UtcNow
-    let trade = {OrderId=id;Price= 10.0m;Quantity= 3;Occured= occured}
-    let expected = {Id = id; Symbol = "TT"; Price= 10.0m; Quantity = 11; TradedQuantity = 3; 
+    let trade = {OrderId=id;Price= 10.0m;Quantity= 3u;Occured= occured}
+    let expected = {Id = id; Symbol = "TT"; Price= 10.0m; Quantity = 11u; TradedQuantity = 3u; 
                     Direction= TradeDirection.Buy; UserId =1; Occured= occured ; Status= OrderStatus.PartiallyFilled
                     Trades= [trade] }
-    let accepted = OrderAccepted(id, "TT", 10.0m, 11, TradeDirection.Buy, occured, 1 )
-    let traded = OrderTraded(trade.OrderId, trade.Price, trade.Quantity, trade.Occured, 1 )
+    let accepted = OrderAccepted(id, "TT", 10.0m, 11u, TradeDirection.Buy, occured, 1u )
+    let traded = OrderTraded(trade.OrderId, trade.Price, trade.Quantity, trade.Occured, 1u )
     let actual = seq { yield accepted; yield traded } |> aggregate
 
     Assert.Equal(expected, actual.Value)
@@ -43,14 +43,14 @@ let ``Order with Create Pending and Traded Event Received Resulting To Partially
 let ``Order with Create Pending and Traded Events Received Resulting To FullyFilled``() =
     let id = Guid.NewGuid()
     let occured = DateTimeOffset.UtcNow
-    let trade1 = {OrderId=id;Price= 10.0m;Quantity= 3;Occured= DateTimeOffset.UtcNow}
-    let trade2 = {OrderId=id;Price= 10.0m;Quantity= 8;Occured= occured}
-    let expected = {Id = id; Symbol = "TT"; Price= 10.0m; Quantity = 11; TradedQuantity = 11; 
+    let trade1 = {OrderId=id;Price= 10.0m;Quantity= 3u;Occured= DateTimeOffset.UtcNow}
+    let trade2 = {OrderId=id;Price= 10.0m;Quantity= 8u;Occured= occured}
+    let expected = {Id = id; Symbol = "TT"; Price= 10.0m; Quantity = 11u; TradedQuantity = 11u; 
                     Direction= TradeDirection.Buy; UserId =1; Occured= occured ; 
                     Status= OrderStatus.FullyFilled; Trades= [trade1;trade2]}
-    let accepted = OrderAccepted(id, "TT", 10.0m, 11, TradeDirection.Buy, DateTimeOffset.UtcNow, 1 )
-    let traded1 = OrderTraded(trade1.OrderId, trade1.Price, trade1.Quantity, trade1.Occured, 1 )
-    let traded2 = OrderTraded(trade2.OrderId, trade2.Price, trade2.Quantity, trade2.Occured, 1 )
+    let accepted = OrderAccepted(id, "TT", 10.0m, 11u, TradeDirection.Buy, DateTimeOffset.UtcNow, 1u )
+    let traded1 = OrderTraded(trade1.OrderId, trade1.Price, trade1.Quantity, trade1.Occured, 1u )
+    let traded2 = OrderTraded(trade2.OrderId, trade2.Price, trade2.Quantity, trade2.Occured, 1u )
     let actual = seq { yield accepted; yield traded1; yield traded2 } |> aggregate
 
     Assert.Equal(expected.TradedQuantity, actual.Value.TradedQuantity)
@@ -64,14 +64,14 @@ let ``Order with Create Pending and Traded Events Received Resulting To FullyFil
 let ``Order with Create Pending and Traded Events Received Resulting To OverFilled``() =
     let id = Guid.NewGuid()
     let occured = DateTimeOffset.UtcNow
-    let trade1 = {OrderId=id;Price= 10.0m;Quantity= 3;Occured= DateTimeOffset.UtcNow}
-    let trade2 = {OrderId=id;Price= 10.0m;Quantity= 9;Occured= occured}
-    let expected = {Id = id; Symbol = "TT"; Price= 10.0m; Quantity = 11; TradedQuantity = 12; 
+    let trade1 = {OrderId=id;Price= 10.0m;Quantity= 3u;Occured= DateTimeOffset.UtcNow}
+    let trade2 = {OrderId=id;Price= 10.0m;Quantity= 9u;Occured= occured}
+    let expected = {Id = id; Symbol = "TT"; Price= 10.0m; Quantity = 11u; TradedQuantity = 12u; 
                     Direction= TradeDirection.Buy; UserId =1; Occured= occured ; 
                     Status= OrderStatus.FullyFilled; Trades= [trade1;trade2]}
-    let accepted = OrderAccepted(id, "TT", 10.0m, 11, TradeDirection.Buy, DateTimeOffset.UtcNow, 1 )
-    let traded1 = OrderTraded(trade1.OrderId, trade1.Price, trade1.Quantity, trade1.Occured, 1 )
-    let traded2 = OrderTraded(trade2.OrderId, trade2.Price, trade2.Quantity, trade2.Occured, 1 )
+    let accepted = OrderAccepted(id, "TT", 10.0m, 11u, TradeDirection.Buy, DateTimeOffset.UtcNow, 1u )
+    let traded1 = OrderTraded(trade1.OrderId, trade1.Price, trade1.Quantity, trade1.Occured, 1u )
+    let traded2 = OrderTraded(trade2.OrderId, trade2.Price, trade2.Quantity, trade2.Occured, 1u )
     let actual = seq { yield accepted; yield traded1; yield traded2 } |> aggregate
 
     Assert.Equal(expected.TradedQuantity, actual.Value.TradedQuantity)
